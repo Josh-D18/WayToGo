da8118ef34d340b1a5b78be313a84e6366de155f
Josh D <jjdate@live.com>
2026-02-04 00:02:44 -0500

    (feat): added in the bell and moon icons in the settings

--- components/SettingsSlider/SettingsSlider.tsx
+++ components/SettingsSlider/SettingsSlider.tsx
@@ -1,8 +1,8 @@
 import { getWindowHeight, getWindowWidth } from "@/helpers/dimensions";
 import { WayToGoTheme } from "@/theme-rn";
-import { CircleX } from "lucide-react-native";
+import { Bell, CircleX, Moon } from "lucide-react-native";
 import React from "react";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Switch, Text, View } from "react-native";
 
 interface SettingsSliderProps {
   active: boolean;
@@ -24,6 +24,26 @@ export const SettingsSlider = (props: SettingsSliderProps) => {
         </View>
         <View style={styles.textContainer}>
           <Text style={styles.text}>Settings</Text>
+        </View>
+        <View style={styles.settingOptionsContainer}>
+          <View style={styles.settingContentContainer}>
+            <Bell style={styles.icon} />
+            <Text style={styles.optionsText}>Notifications</Text>
+            <Switch
+              trackColor={{ false: "#767577", true: "#81b0ff" }}
+              ios_backgroundColor="#3e3e3e"
+              style={styles.switch}
+            />
+          </View>
+          <View style={styles.settingContentContainer}>
+            <Moon style={styles.icon} />
+            <Text style={styles.optionsText}>Dark Mode</Text>
+            <Switch
+              trackColor={{ false: "#767577", true: "#81b0ff" }}
+              ios_backgroundColor="#3e3e3e"
+              style={styles.switch}
+            />
+          </View>
         </View>
       </View>
     </View>
@@ -51,6 +71,7 @@ const styles = StyleSheet.create({
 
   textContainer: {
     alignSelf: "center",
+    marginBottom: 8,
   },
 
   closeIconContainer: {
@@ -62,5 +83,29 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: 600,
     marginRight: 10,
+  },
+  settingContentContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 17,
+    width: "100%",
+  },
+  switch: {
+    marginRight: 20,
+    alignSelf: "flex-end",
+  },
+
+  icon: {
+    marginHorizontal: 20,
+  },
+
+  optionsText: {
+    fontSize: 19,
+    fontWeight: 600,
+  },
+
+  settingOptionsContainer: {
+    marginTop: 25,
   },
 });
